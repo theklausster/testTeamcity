0d2e4ad6cc896f8659d0105c7791e2f374841535
Jakobsen <theklausster@gmail.com>
2015-11-10 08:56:10 +0100

    lol

---
+++ FakeTestLib/FakeTestLib/TheFakeTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace FakeTestLib
+{
+    [TestFixture]
+    class TheFakeTest
+    {
+
+        [Test]
+        public void The_fake_test()
+        {
+            Assert.True(true);
+        }
+    }
+}
